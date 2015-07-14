8ee89880338c4d76ef943821fe176dd3af65e18b
LuXJ <358231333@qq.com>
2015-07-14 09:28:51 +0800

    添加时间戳

--- mylibrary2/src/main/java/com/util/luxj/mylibrary/Public_function.java
+++ mylibrary2/src/main/java/com/util/luxj/mylibrary/Public_function.java
@@ -7,12 +7,11 @@ import android.widget.Toast;
  * Created by Luxj on 2015/7/13.
  */
 public class Public_function {
-    public static void getTime(Context context)
-    {
-        Toast.makeText(context,context.getString(R.string.app_name1),Toast.LENGTH_LONG).show();
-        System.out.println("now is pm++++++++++++"+Test.getNum());
+    public static void getTime(Context context) {
+        Toast.makeText(context, context.getString(R.string.app_name1), Toast.LENGTH_LONG).show();
+        System.currentTimeMillis();
+        System.out.println("now is " + Test.refFormatNowDate());
     }
-
 
 
 }
--- mylibrary2/src/main/java/com/util/luxj/mylibrary/Test.java
+++ mylibrary2/src/main/java/com/util/luxj/mylibrary/Test.java
@@ -1,5 +1,8 @@
 package com.util.luxj.mylibrary;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * Created by Luxj on 2015/7/13.
  */
@@ -7,5 +10,12 @@ public class Test {
 
     public static int getNum() {
         return 1000;
+    }
+
+    public static String refFormatNowDate() {
+        Date nowTime = new Date(System.currentTimeMillis());
+        SimpleDateFormat sdFormatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String retStrFormatNowDate = sdFormatter.format(nowTime);
+        return retStrFormatNowDate;
     }
 }
